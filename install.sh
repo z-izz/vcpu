cdc191489bfd5278d24710c4560ef5d375d3f502
Winksplorer <winksplorer@gordae.com>
2023-04-23 00:41:40 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+sudo cp mkvcpu /bin
+sudo cp mkvhd /bin
+sudo cp vcpu /bin
+sudo cp rmvcpu /bin
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+sudo rm /bin/mkvcpu
+sudo rm /bin/mkvhd
+sudo rm /bin/vcpu
+sudo rm /bin/rmvcpu
